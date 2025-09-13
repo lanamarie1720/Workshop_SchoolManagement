6833ec6b97994c9a8e1343a2d0cd5a0e7190a55f
LanaMB <lanamarie1720@gmail.com>
2025-09-13 15:02:21 +0200

    Course and Student Classes commit

---
+++ src/main/java/se/lexicon/model/Course.java
@@ -0,0 +1,64 @@
+package se.lexicon.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Course {
+
+    private int id;
+    private String courseName;
+    private LocalDate startDate;
+    private int weekDuration;
+    private ArrayList<Student> students = new ArrayList<>();
+
+    public Course(String courseName, LocalDate startDate, int weekDuration) {
+        this.courseName = courseName;
+        this.startDate = startDate;
+        this.weekDuration = weekDuration;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getWeekDuration() {
+        return weekDuration;
+    }
+
+    public void setWeekDuration(int weekDuration) {
+        this.weekDuration = weekDuration;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public void register(Student student){
+        students.add(student);
+    }
+
+    public void unregister(Student student){
+        students.remove(student);
+    }
+
+}
